1587da6abf549d9b2e4c06e26ce53da9cf0b74a0
Orry Baram <orrybaram@gmail.com>
2014-02-18 20:46:50 -0500

    n=game over with commas

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -85,7 +85,7 @@ window.addEventListener("load", function (e) {
 
             $('#game_over_score')
                 .show()
-                .text(stage.options.score);
+                .text(numberWithCommas(stage.options.score));
         }
 
         // RETURNING PLAYER 
